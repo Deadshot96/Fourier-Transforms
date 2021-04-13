3c90641f9af698a0d1f1a1c327470660e24fa20b
Bhushan <bhushan1810patil@gmail.com>
2021-04-14 02:49:28 +0530

    added a few settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+WIDTH = 1000
+HEIGHT = 650
+WIN_WIDTH = 900
+WIN_HEIGHT = 550
+X_OFF = (WIDTH - WIN_WIDTH) // 2
+Y_OFF = int((HEIGHT - WIN_HEIGHT) * 0.8)
+FPS = 60
+MIDBLACK = (51, 51, 51)
+STEEL_BLUE = (70, 130, 180)
+GOLD = (255, 215, 0)
+TITLE_FONT = "comicsansms"
+TITLE_TEXT = "Square Wave - Fourier"
