595a38037f978849c1590443ddcc9e31a710276c
Bhushan <bhushan1810patil@gmail.com>
2021-04-15 00:48:20 +0530

    added SawToothWave class boilerplate

---
+++ Saw Tooth Wave/saw-tooth wave.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+class SawToothWave(object):
+
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    X = SawToothWave()
+    X.run()
