3219f6d789792277b48fe8cd7d34d21a0157dde2
Bhushan <bhushan1810patil@gmail.com>
2021-04-19 23:10:01 +0530

    added epiCycle and drawing and point repetition avoidance logic

--- Arbitrary Fourier/main.py
+++ Arbitrary Fourier/main.py
@@ -24,7 +24,7 @@ class Main:
         self.dftPoints = list()
         self.drawFlag = True
         self.time = 0
-        self.dt = 0.05
+        self.dt = 0
         self.centrePos = self.win_width // 2, self.win_height // 2
         self.path = list()
 
@@ -39,8 +39,7 @@ class Main:
         pygame.display.set_caption("Arbitrary Fourier Tranform")
         self.win.fill(MIDBLACK)
 
-        self.gameRect = pygame.Rect((self.xoff, self.yoff, self.win_width, self.win_height))
-        self.gameWin = self.win.subsurface(self.gameRect)
+        self.gameWin = self.win.subsurface((self.xoff, self.yoff, self.win_width, self.win_height))
         self.gameWin.fill(STEEL_BLUE)
 
         self.clock = pygame.time.Clock()
@@ -55,19 +54,40 @@ class Main:
         self.win.blit(title, (blitX, blitY))
         pygame.display.update()
 
-    def epiCycle(self):
-        pass
+    def epiCycle(self, x, y, angle):
+        for index, ptObj in enumerate(self.dftPoints):
+            prevX, prevY = x, y
+            
+            x += ptObj.amp * math.cos(ptObj.freq * self.time + ptObj.phase + angle)
+            y += ptObj.amp * math.sin(ptObj.freq * self.time + ptObj.phase + angle)
+
+            if index:
+                pygame.draw.circle(self.gameWin, DIM_WHITE, (prevX, prevY), ptObj.amp, 1)
+                pygame.draw.line(self.gameWin, DIM_WHITE, (prevX, prevY), (x, y), 1)
+
+        pygame.display.update()
+        return x, y
 
     def draw(self):
         if self.dftPoints:
             self.gameWin.fill(STEEL_BLUE)
 
-            x, y = self.centrePos
+            x, y = 0, 0
+            x, y = self.epiCycle(x, y, 0)
+            self.path.insert(0, (x, y))
 
-            for i in range(len(self.points)):     
-                prevX, prevY = x, y
+            px, py = self.path[0]
 
+            for i in range(1, len(self.path) - 1):
+                x, y = self.path[i]
+                pygame.draw.line(self.gameWin, MID_WHITE, (px, py), (x, y), 2)          
+                px, py = x, y
 
+            self.time += self.dt
+
+            if self.time >= (math.pi * 2):
+                self.time = 0
+                self.path.clear()
 
         pygame.display.update()
 
@@ -76,8 +96,10 @@ class Main:
         for k in range(len(self.points)):
             total = complex(0, 0)
             for n in range(len(self.points)):
-                exp = complex(0, -2 * math.pi * k * n / len(self.points))
-                total += (self.points[n] * math.e ** exp)
+                # phi = complex(0, -2 * math.pi * k * n / len(self.points))
+                phi = -2 * math.pi * k * n / len(self.points)
+                mul = complex(math.cos(phi), -math.sin(phi))
+                total += (self.points[n] * mul)
 
             total /= len(self.points)
             re = total.real
@@ -86,11 +108,12 @@ class Main:
             freq = k
             phase = math.atan2(im, re)
 
-            obj = self.dftPointObj(re, im, amp, freq, phase)
+            obj = self.dftPointObj(re=re, im=im, amp=amp, freq=freq, phase=phase)
             self.dftPoints.append(obj) 
 
         self.dftPoints.sort(key=lambda x: x.amp, reverse=True)
-
+        self.time = 0
+        self.dt = 2 * math.pi / len(self.dftPoints)
 
     def quit(self):
         pygame.font.quit()
@@ -102,28 +125,23 @@ class Main:
         if not pygame.display.get_init():
             self.display_init()
 
+        self.gameRect = pygame.Rect((0, 0, self.win_width, self.win_height))
+
         run = True
         while run:
-            self.clock.tick(self.fps)
+            if self.drawFlag:
+                self.clock.tick(self.fps)
+            else:
+                self.clock.tick(self.fps // 3)
 
             for event in pygame.event.get():
 
                 if event.type == pygame.QUIT:
                     run = False
 
-                # if event.type == pygame.MOUSEBUTTONDOWN:
-                #     x, y = pygame.mouse.get_pos()
-
-                #     if self.drawFlag and self.gameRect.collidepoint(x - self.xoff, y - self.yoff):
-                #         x -= self.xoff
-                #         y -= self.yoff
-                #         print("In MOUSEBUTTONDOWN: ", x, y, sep="\t")
-                #         self.gameWin.set_at((x, y), MID_WHITE)
-                #         pygame.display.update()
-                #         self.points.append((x, y))
-
                 if event.type == pygame.MOUSEBUTTONUP:
                     self.drawFlag = False
+                    self.points.reverse()
 
                 if event.type == pygame.KEYDOWN:
                     
@@ -136,19 +154,21 @@ class Main:
                         
                     if event.key == pygame.K_RETURN and not self.drawFlag:
                         self.dft()
-                        print(self.dftPoints)
-
+                        
             pressed = pygame.mouse.get_pressed()
-
+            px, py = -1, -1
             if pressed[0]:
                     x, y = pygame.mouse.get_pos()
+                    x -= self.xoff
+                    y -= self.yoff
+                    flag = (px != x) and (py != y)
 
-                    if self.drawFlag and self.gameRect.collidepoint(x - self.xoff, y - self.yoff):
-                        x -= self.xoff
-                        y -= self.yoff
+                    if self.drawFlag and self.gameRect.collidepoint(x, y) and flag:
+
                         self.gameWin.set_at((x, y), MID_WHITE)
                         pt = complex(x, y)
                         self.points.append(pt)
+                        px, py = x, y
 
                         if len(self.points) > 2:
                             p1 = self.points[-1].real, self.points[-1].imag
