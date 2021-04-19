95309f0b64ee82a2c91275e49a48ff1b430fe91b
Bhushan <bhushan1810patil@gmail.com>
2021-04-19 14:28:05 +0530

    added declaration of class variables and functions

--- Arbitrary Fourier/main.py
+++ Arbitrary Fourier/main.py
@@ -5,6 +5,25 @@ from settings import *
 class Main:
 
     def __init__(self):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.gameWidth = WIN_WIDTH
+        self.gameHeight = WIN_HEIGHT
+        self.xoff = X_OFF
+        self.yoff = Y_OFF
+        self.fps = FPS
+        self.win = None
+        self.gameWidth = None
+        self.clock = None
+        self.titleFont = None
+
+    def display_init(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def quit(self):
         pass
 
     def run(self):
