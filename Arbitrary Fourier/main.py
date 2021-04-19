f731d846e1ea4a23748783cb883e3f573febef0b
Bhushan <bhushan1810patil@gmail.com>
2021-04-19 14:37:45 +0530

    added display_init

--- Arbitrary Fourier/main.py
+++ Arbitrary Fourier/main.py
@@ -13,12 +13,36 @@ class Main:
         self.yoff = Y_OFF
         self.fps = FPS
         self.win = None
+        self.gameWin = None
         self.gameWidth = None
         self.clock = None
         self.titleFont = None
 
     def display_init(self):
-        pass
+
+        pygame.init()
+        pygame.display.init()
+        pygame.font.init()
+
+        winDims = self.width, self.height
+        self.win = pygame.display.set_mode(winDims)
+        self.win.fill(MIDBLACK)
+
+        gameRect = self.xoff, self.yoff, self.gameWidth, self.gameHeight
+        self.gameWin = self.win.subsurface(gameRect)
+        self.gameWin.fill(STEEL_BLUE)
+
+        self.clock = pygame.time.Clock()
+
+        self.titleFont = pygame.font.SysFont(TITLE_FONT, TITLE_FONT_SIZE)
+        title = self.titleFont.render(TITLE_TEXT, 1, GOLD)
+        w, h = title.get_size()
+
+        blitX = (self.width - w) // 2
+        blitY = (self.yoff - h) // 2
+
+        self.win.blit(title, (blitX, blitY))
+
 
     def draw(self):
         pass
