563385e84298ee3c0d2cfbc91d7972587ed5869c
Bhushan <bhushan1810patil@gmail.com>
2021-04-19 14:24:28 +0530

    added Main class declaration

--- Arbitrary Fourier/main.py
+++ Arbitrary Fourier/main.py
@@ -2,3 +2,15 @@ import pygame
 import math
 from settings import *
 
+class Main:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    X = Main()
+    X.run()
